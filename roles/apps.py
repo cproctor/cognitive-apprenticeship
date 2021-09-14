cc839f6e4c922abb086fad66fc489741ff6c8fdc
Chris Proctor <chris@chrisproctor.net>
2021-09-14 13:02:09 -0400

    Profile signal now being connected properly

--- roles/apps.py
+++ roles/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RolesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'roles'
+
+    def ready(self):
+        "Connects signals when app is ready"
+        import roles.signals
--- roles/models.py
+++ roles/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    is_author = models.BooleanField()
-    is_reviewer = models.BooleanField()
-    is_editor = models.BooleanField()
+    is_author = models.BooleanField(default=False)
+    is_reviewer = models.BooleanField(default=False)
+    is_editor = models.BooleanField(default=False)
     needs_teacher_review = models.BooleanField(default=True)
 
     def __str__(self):

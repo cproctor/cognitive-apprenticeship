925684811933bdc047424e8e8ef23ebf234a1951
Chris Proctor <chris@chrisproctor.net>
2021-09-15 14:18:44 -0400

    Clarified terms in profile creation signal

--- roles/signals.py
+++ roles/signals.py
@@ -3,9 +3,9 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
-@receiver(post_save, sender=User, dispatch_uid="literacy_event_notification")
-def user_signed_up_events(sender, **kwargs):
-    "When a user signs up, create a profile and a literacy event"
+@receiver(post_save, sender=User, dispatch_uid="create_profile_after_user_signup")
+def create_profile_after_user_signup(sender, **kwargs):
+    "When a user signs up, create a profile"
     user = kwargs['instance']
     if kwargs['created']:
         Profile.objects.create(user=user)

ca322f97767b1ad0c67307b1ead35e3de9990251
Chris Proctor <chris@chrisproctor.net>
2021-09-19 21:14:12 -0400

    Added public view of issues

--- public/views.py
+++ public/views.py
@@ -1,11 +1,14 @@
 from django.views.generic.base import TemplateView
+from editor.models import JournalIssue
+from django.conf import settings
 
 class HomePage(TemplateView):
     template_name = "public/home_page.html"
 
     def get_context_data(self):
         return {
-          'articles': []
+            'issues': JournalIssue.objects.filter(published=True).all(),
+            'editor_names': settings.EDITOR_NAMES,
         }
 
 class AboutPage(TemplateView):

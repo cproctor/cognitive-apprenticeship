bcd3962c40827e7746e39ba9485976dfae01fc0a
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-08-31 11:19:44 -0400

    WSGI points to production settings

--- cognitive_apprenticeship/wsgi.py
+++ cognitive_apprenticeship/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cognitive_apprenticeship.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cognitive_apprenticeship.settings_production')
 
 application = get_wsgi_application()
